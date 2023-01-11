ac2607d145955c7e394a6f550abf0f9053f16550
glory jacques <101422810+lonniemoss@users.noreply.github.com>
2023-01-11 16:59:28 -0500

    almost done

--- views.py
+++ views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
+from rest_framework import generics
 from .serializers import EmployeesSerializer
 from .models import Employees
 from rest_framework.views import APIView
@@ -13,31 +14,44 @@ from PIL import Image
 import io
 import base64
 
-image = face_recognition.load_image_file("your_file.jpg")
+image = face_recognition.load_image_file("myapi/images/profile_pic.jpg")
 face_locations = face_recognition.face_locations(image)
 
-class EmployeesRegistrationView(viewsets.CreateAPIView):
+class EmployeesRegistrationView(generics.CreateAPIView):
     queryset = Employees.objects.all()
     serializer_class = EmployeesSerializer
 
 class EmployeesRecognitionView(APIView):
     def post(self, request, format=None):
-        picture_of_me = face_recognition.load_image_file("me.jpg")
-        my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
+        image = request.data['image']
+        image = Image.open(io.BytesIO(base64.b64decode(image)))
+        image = np.array(image)
+        image = face_recognition.load_image_file(image)
+        face_locations = face_recognition.face_locations(image)
 
-        # my_face_encoding now contains a universal 'encoding' of my facial features that can be compared to any other picture of a face!
+        employees = Employees.objects.all()
+        for employee in employees:
+            employee_image = face_recognition.load_image_file(employee.image)
+            employee_face_locations = face_recognition.face_locations(employee_image)
+            employee_face_encoding = face_recognition.face_encodings(employee_image, employee_face_locations)[0]
+            results = face_recognition.compare_faces([employee_face_encoding], image)
+            if results[0]:
+                return Response({
+                    'message': 'Employee recognized',
+                    'employee': {
+                        'id': employee.id,
+                        'first_name': employee.first_name,
+                        'last_name': employee.last_name,
+                        'role': employee.role,
+                        'email': employee.email,
+                        'phone': employee.phone,
+                        'image': employee.image
+                    }
+                }, status=status.HTTP_200_OK)
 
-        unknown_picture = face_recognition.load_image_file("unknown.jpg")
-        unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
-
-        # Now we can see the two face encodings are of the same person with `compare_faces`!
-
-        results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)
-
-        if results[0] == True:
-            print("It's a picture of me!")
-        else:
-            print("It's not a picture of me!")
+        return Response({
+            'message': 'Employee not recognized'
+        }, status=status.HTTP_404_NOT_FOUND)
 
 
 
